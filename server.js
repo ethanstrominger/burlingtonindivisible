49bae93374c1fb0ddfc99e1fb0e62a39869504df
Ethan Strominger <ethanstrominger2@gmail.com>
2026-06-11 03:30:54 -0400

    Modify server.js to retrigger deploy

--- server.js
+++ server.js
@@ -4,6 +4,7 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+
 const cors = require("cors");
 const { Pool } = require("pg");
 // Load environment variables from .env in development
